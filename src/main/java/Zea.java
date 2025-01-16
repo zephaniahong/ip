cc3a7cbd3d5ff4923f6c5d2b0c0356e79829a960
Zephaniah Ong <zealorez@gmail.com>
2025-01-16 20:20:15 +0800

    Implement echo

--- src/main/java/Zea.java
+++ src/main/java/Zea.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Zea {
     public static void main(String[] args) {
-        String logo = "Hello! I'm Zea\n"
-                + "What can I do for you?\n"
-                + "______________________\n"
-                + "Bye. Hope to see you again soon!\n";
-        System.out.println(logo);
-  }
+            String logo = "______________________\n"
+                    + "Hello! I'm Zea\n"
+                    + "What can I do for you?\n"
+                    + "______________________\n";
+
+            System.out.println(logo);
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String command = scanner.nextLine();
+                if (command.equals("bye")) {
+                    System.out.println("----------------------");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("----------------------");
+                    break;
+                } else {
+                    System.out.println("----------------------");
+                    System.out.println(command);
+                    System.out.println("----------------------");
+                }
+            }
+    }
 }
