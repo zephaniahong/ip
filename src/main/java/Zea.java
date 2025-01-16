9b3fdb619ad2b55ac81c7c8dc8799285a9a222dc
Zephaniah Ong <zealorez@gmail.com>
2025-01-16 20:27:31 +0800

    Implement add and list

--- src/main/java/Zea.java
+++ src/main/java/Zea.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Zea {
     public static void main(String[] args) {
+        ArrayList<String> store = new ArrayList<>();
             String logo = "______________________\n"
                     + "Hello! I'm Zea\n"
                     + "What can I do for you?\n"
@@ -12,14 +14,22 @@ public class Zea {
             Scanner scanner = new Scanner(System.in);
             while (true) {
                 String command = scanner.nextLine();
-                if (command.equals("bye")) {
+                if (command.equals("bye")) { // Exit from program
                     System.out.println("----------------------");
                     System.out.println("Bye. Hope to see you again soon!");
                     System.out.println("----------------------");
                     break;
-                } else {
+                } else if (command.equals("list")) { // Print out all commands in store
                     System.out.println("----------------------");
-                    System.out.println(command);
+                    for (int i = 0; i < store.size(); i++) {
+                        String c = store.get(i);
+                        System.out.println(i+1 + ". " + c);
+                    }
+                    System.out.println("----------------------");
+                } else { // add command to store
+                    store.add(command);
+                    System.out.println("----------------------");
+                    System.out.println("added: " + command);
                     System.out.println("----------------------");
                 }
             }
