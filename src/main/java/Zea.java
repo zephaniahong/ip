e0a3279335e1dd02a93a6b7815e5b9330ef57915
Zephaniah Ong <zealorez@gmail.com>
2025-01-16 20:48:47 +0800

    Implement marking task as done and undone

--- src/main/java/Zea.java
+++ src/main/java/Zea.java
@@ -3,31 +3,49 @@ import java.util.Scanner;
 
 public class Zea {
     public static void main(String[] args) {
-        ArrayList<String> store = new ArrayList<>();
+        ArrayList<Task> store = new ArrayList<>();
             String logo = "______________________\n"
                     + "Hello! I'm Zea\n"
                     + "What can I do for you?\n"
                     + "______________________\n";
-
             System.out.println(logo);
 
             Scanner scanner = new Scanner(System.in);
             while (true) {
                 String command = scanner.nextLine();
-                if (command.equals("bye")) { // Exit from program
+                String[] commands = command.split(" ");
+                if (commands[0].equals("bye")) { // Exit from program
                     System.out.println("----------------------");
                     System.out.println("Bye. Hope to see you again soon!");
                     System.out.println("----------------------");
                     break;
-                } else if (command.equals("list")) { // Print out all commands in store
+                } else if (commands[0].equals("list")) { // Print out all commands in store
                     System.out.println("----------------------");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < store.size(); i++) {
-                        String c = store.get(i);
-                        System.out.println(i+1 + ". " + c);
+                        Task t = store.get(i);
+                        System.out.println(i+1 + ". " + t);
                     }
                     System.out.println("----------------------");
+                } else if (commands[0].equals("mark")) { // TODO: handle invalid idx
+                    int idx = Integer.parseInt(commands[1]) - 1; // subtract 1 to convert list number to array idx
+                    Task t = store.get(idx);
+                    t.done();
+                    System.out.println("----------------------");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(t);
+                    System.out.println("----------------------");
+                } else if (commands[0].equals("unmark")) { // TODO: handle invalid idx
+                    int idx = Integer.parseInt(commands[1]) - 1; // subtract 1 to convert list number to array idx
+                    Task t = store.get(idx);
+                    t.undone();
+                    System.out.println("----------------------");
+                    System.out.println("Ok, I've marked this task as not done yet:");
+                    System.out.println(t);
+                    System.out.println("----------------------");
                 } else { // add command to store
-                    store.add(command);
+                    Task newTask = new Task(command);
+                    store.add(newTask);
                     System.out.println("----------------------");
                     System.out.println("added: " + command);
                     System.out.println("----------------------");
