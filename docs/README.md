85e8b3cff0476e736da1f23e9f5cefc6927be834
Zephaniah Ong <zealorez@gmail.com>
2025-01-16 20:08:47 +0800

    Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Zea User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Zea.java
@@ -0,0 +1,9 @@
+public class Zea {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm Zea\n"
+                + "What can I do for you?\n"
+                + "______________________\n"
+                + "Bye. Hope to see you again soon!\n";
+        System.out.println(logo);
+  }
+}
