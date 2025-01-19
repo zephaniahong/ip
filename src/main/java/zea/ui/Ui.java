c8ca3c2ca087c8fcd0b695b579b6372bbb4e3eff
Zephaniah Ong <zealorez@gmail.com>
2025-01-20 07:29:52 +0800

    Add JavaDocs

--- src/main/java/zea/ui/Ui.java
+++ src/main/java/zea/ui/Ui.java
@@ -12,6 +12,9 @@ public class Ui {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Display the welcome message
+     */
     public void showWelcome() {
         String logo = "______________________\n"
                 + "Hello! I'm Zea.Zea\n"
@@ -20,12 +23,24 @@ public class Ui {
         System.out.println(logo);
     }
 
+    /**
+     * Displays the loading error message to the user
+     * @param e - the error to be displayed
+     */
     public void showLoadingError(ZeaException e) {
         System.out.println(e.getMessage());
     }
 
+    /**
+     * Displays a single item along with a header and footer
+     * @param header - a message before the item is displayed
+     * @param item - the item to be displayed
+     * @param footer - a message after the item is displayed
+     * @param <T> - any type that can be printed
+     */
     public <T>void displayItem(String header, T item, String footer) {
         System.out.println("----------------------");
+        System.out.println(header);
         System.out.println(item);
         if (!Objects.equals(footer, "")) {
             System.out.println(footer);
@@ -33,6 +48,12 @@ public class Ui {
         System.out.println("----------------------");
     }
 
+    /**
+     * Displays all the items with numbering
+     * @param header- a message before the item is displayed
+     * @param list - a list of items to be printed
+     * @param <T>- any type that can be printed
+     */
     public <T> void displayWithNumbering(String header, ArrayList<T> list) {
         System.out.println("----------------------");
         System.out.println(header);
@@ -49,6 +70,10 @@ public class Ui {
         System.out.println("----------------------");
     }
 
+    /**
+     * Reads the next line of the standard input
+     * @return the input given by the user via stdin
+     */
     public String readCommand() {
         return this.scanner.nextLine();
     }
